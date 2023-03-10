5af29da78eab8f376587009f2e79f19183c5d190
inpartec.fflopes <inpartec.fflopes@terceiros.mag.com.br>
2023-03-10 12:40:57 -0300

    added styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,27 @@
+import Image from 'next/image';
 import { styled } from '../styles';
+import { HomeContainer, Product } from '../styles/pages/home';
 
-const Button = styled('button', {
-	backgroundColor: '$rocketSeat',
-	borderRadius: 4,
-	border: 0,
-	padding: '4px 8px',
+import shirtOne from '../assets/shirts/1.png';
+import shirtTwo from '../assets/shirts/2.png';
 
-	'&:hover': {
-		filter: 'brightness(.8)',
-	},
-});
 export default function Home() {
 	return (
-		<>
-			<Button>Hello</Button>
-		</>
+		<HomeContainer>
+			<Product>
+				<Image src={shirtOne} alt="" width={520} height={480} />
+				<footer>
+					<strong> Shirt x</strong>
+					<span> $29.90</span>
+				</footer>
+			</Product>
+			<Product>
+				<Image src={shirtTwo} alt="" width={520} height={480} />
+				<footer>
+					<strong> Shirt x</strong>
+					<span> $29.90</span>
+				</footer>
+			</Product>
+		</HomeContainer>
 	);
 }
