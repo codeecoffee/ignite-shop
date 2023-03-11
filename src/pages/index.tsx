15d40ed2b5b333cfdcbc8ef681010fbd4843c210
inpartec.fflopes <inpartec.fflopes@terceiros.mag.com.br>
2023-03-10 23:59:10 -0300

    added carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
 import Image from 'next/image';
 import { styled } from '../styles';
 import { HomeContainer, Product } from '../styles/pages/home';
@@ -6,16 +8,36 @@ import shirtOne from '../assets/shirts/1.png';
 import shirtTwo from '../assets/shirts/2.png';
 
 export default function Home() {
+	const [slideRef] = useKeenSlider({
+		slides: {
+			perView: 3,
+			spacing: 48,
+		},
+	});
 	return (
-		<HomeContainer>
-			<Product>
+		<HomeContainer ref={slideRef} className="keen-slider">
+			<Product className="keen-slider__slide">
 				<Image src={shirtOne} alt="" width={520} height={480} />
 				<footer>
 					<strong> Shirt x</strong>
 					<span> $29.90</span>
 				</footer>
 			</Product>
-			<Product>
+			<Product className="keen-slider__slide">
+				<Image src={shirtTwo} alt="" width={520} height={480} />
+				<footer>
+					<strong> Shirt x</strong>
+					<span> $29.90</span>
+				</footer>
+			</Product>
+			<Product className="keen-slider__slide">
+				<Image src={shirtTwo} alt="" width={520} height={480} />
+				<footer>
+					<strong> Shirt x</strong>
+					<span> $29.90</span>
+				</footer>
+			</Product>
+			<Product className="keen-slider__slide">
 				<Image src={shirtTwo} alt="" width={520} height={480} />
 				<footer>
 					<strong> Shirt x</strong>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,7 @@ import { styled } from '..';
 
 export const HomeContainer = styled('main', {
 	display: 'flex',
-	gap: '3rem',
+	// gap: '3rem',
 	width: '100%',
 	maxWidth: 'calc(100vw-((100vw- 1180px)/2))',
 	marginLeft: 'auto',
@@ -10,7 +10,7 @@ export const HomeContainer = styled('main', {
 });
 export const Product = styled('div', {
 	borderRadius: 8,
-	padding: '.25rem',
+	// padding: '.25rem',
 	cursor: 'pointer',
 	position: 'relative',
 	background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
